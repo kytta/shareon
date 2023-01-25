6453f15d70b10c8484078e4a165ee78b0c735b95
Nikita Karamov <me@kytta.dev>
2023-01-25 18:46:58 +0100

    Remove banner

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,3 @@
-const package_ = require("./package.json");
-const bannerText = `${package_.name} v${package_.version}`;
-
 module.exports = {
   map: {
     inline: false,
@@ -10,10 +7,5 @@ module.exports = {
     require("postcss-calc"),
     require("autoprefixer"),
     require("postcss-csso"),
-    require("postcss-banner")({
-      banner: bannerText,
-      important: true,
-      inline: true,
-    }),
   ],
 };
