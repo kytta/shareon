adf5d62b7cca53e187e97189b153f80eaaccd9a5
Nikita Karamov <me@kytta.dev>
2023-01-25 18:17:08 +0100

    Minify with esbuild instead of terser
    In the end, the files are basically of the same size, but esbuild is
    faster. We also do not need any extra packages.

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import * as path from "node:path";
 import package_ from "./package.json";
 
 export default defineConfig({
-  esbuild: {
-    minify: true,
-  },
   build: {
     sourcemap: true,
     target: "esnext",
-    minify: "terser",
     lib: {
       entry: path.resolve("./src/index.js"),
       name: "Shareon",
